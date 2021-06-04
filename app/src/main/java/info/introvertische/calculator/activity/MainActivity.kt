468280dc3723c1ece29fd8671b8d6d3c57e10ac8
Alexey Radke <alexeyradke@gmail.com>
2021-06-05 00:15:00 +0700

    Bug fixes

--- app/src/main/java/info/introvertische/calculator/activity/MainActivity.kt
+++ app/src/main/java/info/introvertische/calculator/activity/MainActivity.kt
@@ -1,9 +1,9 @@
 package info.introvertische.calculator.activity
 
 import android.app.AlertDialog
+import android.content.ClipData
 import android.content.Context
 import android.content.DialogInterface
-import android.content.Intent
 import android.content.res.Configuration
 import android.os.Build
 import android.os.Bundle
@@ -12,10 +12,10 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
-import info.introvertische.calculator.BuildConfig
 import info.introvertische.calculator.R
 import info.introvertische.calculator.fragments.BasicNumberPadFragment
 import info.introvertische.calculator.fragments.EngineeringNumberPadFragment
@@ -64,6 +64,7 @@ class MainActivity : AppCompatActivity(), ClickHandler, ClickDeleteHistory, Clic
     private  lateinit var lastFragment: Fragment
     private var isHistory = false
     private var isEngineeringPad = false
+    private var isNightMode = true
     private var instanceState: Bundle? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -342,10 +343,23 @@ class MainActivity : AppCompatActivity(), ClickHandler, ClickDeleteHistory, Clic
         alert.show()
     }
 
-    fun onClickIPCalculator(item: MenuItem?) {
-        val intent = Intent(this, IPActivity::class.java)
-        startActivity(intent)
-    }
+//    fun switchTheme (item: MenuItem?) {
+//        isNightMode = !isNightMode
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P){
+//            if (isNightMode) {
+//                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+//            } else {
+//                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+//            }
+//        } else {
+//            //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+//            //themeSystem.visibility = View.GONE
+//        }
+//    }
+//
+//    private fun setActivityTheme(themeMode: Int) {
+//        AppCompatDelegate.setDefaultNightMode(themeMode)
+//    }
 
     override fun deleteHistory(isDelete: Boolean) {
         if (isDelete) {
